6ff2f879aad1b847fe6e1936e1f52e5179058518
Ali Alkattan <aqattan@Alis-MBP.attlocal.net>
2018-02-09 13:30:30 -0600

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :addresses
   resources :people
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", sessions: 'users/sessions'}
-  root 'people#index'
+  root 'public#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
